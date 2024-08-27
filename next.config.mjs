f8ee4609ef46a288eeb6d34a522b1d64cd850b43
Shulinagarwal <154981191+Shulinagarwal@users.noreply.github.com>
2024-08-28 02:17:56 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['assets.aceternity.com',
+            'images.unsplash.com'
+
+        ],
+    },
+    reactStrictMode: false,
+};
+
+export default nextConfig;
